fb11116a861f42561e3adec4447c759a5df9c0b9
bakhi <bakhi@purdue.edu>
2016-10-03 21:14:18 -0400

    3

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,13 +1,13 @@
 package plugin
 
 import (
-	"github.com/udsfs"
+	"github.com/bakhi/udsfs"
 	"gopkg.in/sensorbee/sensorbee.v0/bql"
 	"gopkg.in/sensorbee/sensorbee.v0/bql/udf"
 )
 
 func init() {
-	bql.MustRegisterGlobalSourceCreator("lorems", bql.SourceCreatorFunc(udsfs.CreateLoremSource))
+	bql.MustRegisterGlobalSourceCreator("lorem", bql.SourceCreatorFunc(udsfs.CreateLoremSource))
 	udf.MustRegisterGlobalUDSFCreator("word_splitter", udf.MustConvertToUDSFCreator(udsfs.CreateWordSplitter))
 	udf.MustRegisterGlobalUDSFCreator("ticker", udf.MustConvertToUDSFCreator(udsfs.CreateTicker))
 }
